dbfbcc549887d760b3981b5c999f2c6343dbabf3
Byrne Hollander <byrnehollander@users.noreply.github.com>
2021-04-12 21:20:37 -0400

    Awkward styling

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import Button from '@material-ui/core/Button'
 import IconButton from '@material-ui/core/IconButton'
 import Slider from '@material-ui/core/Slider'
-import { borders } from '@material-ui/system'
+import { withStyles } from '@material-ui/core/styles'
 import './App.css'
 import cards from './cards.json'
 import { ReactComponent as Black } from './assets/black.svg'
@@ -30,7 +30,6 @@ const hasColor = (card, colors) => {
     return false
   }
   const cardColorsArray = card.colors
-  colors.add('C')
   for (let i = 0; i <= cardColorsArray.length; i++) {
     if (colors.has(cardColorsArray[i])) {
       return true
@@ -38,6 +37,48 @@ const hasColor = (card, colors) => {
   }
   return false
 }
+
+const StyledWhiteIconButton = withStyles({
+  label: {
+    boxShadow: '0 0 12px 5px rgb(254 251 213 / 70%)',
+    borderRadius: 100
+  }
+})(IconButton)
+
+const StyledBlueIconButton = withStyles({
+  label: {
+    boxShadow: '0 0 12px 5px rgb(170 224 250 / 70%)',
+    borderRadius: 100
+  }
+})(IconButton)
+
+const StyledBlackIconButton = withStyles({
+  label: {
+    boxShadow: '0 0 12px 5px rgb(202 194 190 / 70%)',
+    borderRadius: 100
+  }
+})(IconButton)
+
+const StyledRedIconButton = withStyles({
+  label: {
+    boxShadow: '0 0 12px 5px rgb(249 170 143 / 70%)',
+    borderRadius: 100
+  }
+})(IconButton)
+
+const StyledGreenIconButton = withStyles({
+  label: {
+    boxShadow: '0 0 12px 5px rgb(155 211 174 / 70%)',
+    borderRadius: 100
+  }
+})(IconButton)
+
+const StyledColorlessIconButton = withStyles({
+  label: {
+    boxShadow: '0 0 12px 5px rgb(203 193 191 / 70%)',
+    borderRadius: 100
+  }
+})(IconButton)
 
 function App () {
   const [types, setTypes] = useState(new Set(['Instant']))
@@ -66,33 +107,80 @@ function App () {
     setColors(newColors)
   }
 
-  const displayColors = new Set(colors)
-  displayColors.delete('C')
   return (
     <div>
-      <div>Colors: <Colorless width={20} /> {displayColors}</div>
       <b>Instants</b>
       <div>
-        <IconButton border={10} borderColor='black' onClick={() => toggleColor('B')} color='primary' aria-label='Black Mana' component='span'>
-          <Black width={50} />
-        </IconButton>
-        <IconButton onClick={() => toggleColor('U')} color='primary' aria-label='Blue Mana' component='span'>
-          <Blue width={50} />
-        </IconButton>
-        <IconButton onClick={() => toggleColor('G')} color='primary' aria-label='Green Mana' component='span'>
-          <Green width={50} />
-        </IconButton>
-        <IconButton onClick={() => toggleColor('R')} color='primary' aria-label='Red Mana' component='span'>
-          <Red width={50} />
-        </IconButton>
-        <IconButton onClick={() => toggleColor('W')} color='primary' aria-label='White Mana' component='span'>
-          <White width={50} />
-        </IconButton>
+        {colors.has('W')
+          ? (
+            <StyledWhiteIconButton onClick={() => toggleColor('W')} color='primary' aria-label='White Mana' component='span'>
+              <White width={50} />
+            </StyledWhiteIconButton>
+            )
+          : (
+            <IconButton onClick={() => toggleColor('W')} color='primary' aria-label='White Mana' component='span'>
+              <White width={50} />
+            </IconButton>
+            )}
+        {colors.has('U')
+          ? (
+            <StyledBlueIconButton onClick={() => toggleColor('U')} color='primary' aria-label='Blue Mana' component='span'>
+              <Blue width={50} />
+            </StyledBlueIconButton>
+            )
+          : (
+            <IconButton onClick={() => toggleColor('U')} color='primary' aria-label='Blue Mana' component='span'>
+              <Blue width={50} />
+            </IconButton>
+            )}
+        {colors.has('B')
+          ? (
+            <StyledBlackIconButton onClick={() => toggleColor('B')} color='primary' aria-label='Black Mana' component='span'>
+              <Black width={50} />
+            </StyledBlackIconButton>
+            )
+          : (
+            <IconButton onClick={() => toggleColor('B')} color='primary' aria-label='Black Mana' component='span'>
+              <Black width={50} />
+            </IconButton>
+            )}
+        {colors.has('R')
+          ? (
+            <StyledRedIconButton onClick={() => toggleColor('R')} color='primary' aria-label='Red Mana' component='span'>
+              <Red width={50} />
+            </StyledRedIconButton>
+            )
+          : (
+            <IconButton onClick={() => toggleColor('R')} color='primary' aria-label='Red Mana' component='span'>
+              <Red width={50} />
+            </IconButton>
+            )}
+        {colors.has('G')
+          ? (
+            <StyledGreenIconButton onClick={() => toggleColor('G')} color='primary' aria-label='Green Mana' component='span'>
+              <Green width={50} />
+            </StyledGreenIconButton>
+            )
+          : (
+            <IconButton onClick={() => toggleColor('G')} color='primary' aria-label='Green Mana' component='span'>
+              <Green width={50} />
+            </IconButton>
+            )}
+        {colors.has('C')
+          ? (
+            <StyledColorlessIconButton onClick={() => toggleColor('C')} color='primary' aria-label='Colorless Mana' component='span'>
+              <Colorless width={50} />
+            </StyledColorlessIconButton>
+            )
+          : (
+            <IconButton onClick={() => toggleColor('C')} color='primary' aria-label='Colorless Mana' component='span'>
+              <Colorless width={50} />
+            </IconButton>
+            )}
       </div>
 
       <Button variant='contained' color='primary' onClick={() => setTypes(new Set(['Instant']))}>Instant</Button>
       <Button variant='contained' color='primary' onClick={() => setTypes(new Set(['Sorcery']))}>Sorcery</Button>
-      <Button variant='contained' color='primary' onClick={() => setColors(new Set(['C', 'W']))}>W</Button>
       <Slider
         defaultValue={8}
         aria-labelledby='discrete-slider-always'
