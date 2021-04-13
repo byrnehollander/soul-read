13a33ba11780772c3f3e9b555a7892c5ce61435f
Byrne Hollander <byrnehollander@users.noreply.github.com>
2021-04-12 23:26:31 -0400

    Nicer formatting

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import { useState } from 'react'
 import Button from '@material-ui/core/Button'
 import IconButton from '@material-ui/core/IconButton'
 import Slider from '@material-ui/core/Slider'
+import Tooltip from '@material-ui/core/Tooltip'
+import Typography from '@material-ui/core/Typography'
 import { withStyles } from '@material-ui/core/styles'
+import styled from 'styled-components'
 import './App.css'
 import cards from './cards.json'
 import { ReactComponent as Black } from './assets/black.svg'
@@ -11,6 +14,40 @@ import { ReactComponent as Colorless } from './assets/colorless.svg'
 import { ReactComponent as Green } from './assets/green.svg'
 import { ReactComponent as Red } from './assets/red.svg'
 import { ReactComponent as White } from './assets/white.svg'
+
+const Container = styled.div`
+  margin: 40px;
+`
+
+const ButtonContainer = styled.div`
+  margin-top: 40px;
+`
+
+const ColorsHeader = styled.div`
+  display: flex;
+  align-items: end;
+`
+
+const ManaCostContainer = styled.div`
+  margin-top: 40px;
+  margin-left: 60px;
+
+  @media only screen and (max-width: 1020px) {
+    margin-left: 0px;
+  }
+`
+
+const SliderContainer = styled.div`
+  width: 430px;
+`
+
+const OptionsContainer = styled.div`
+  display: flex;
+  margin-bottom: 50px;
+  @media only screen and (max-width: 1020px) {
+    flex-direction: column;
+  }
+`
 
 const hasType = (card, types) => {
   if (!card.types) {
@@ -82,19 +119,107 @@ const StyledColorlessIconButton = withStyles({
 
 function App () {
   const [types, setTypes] = useState(new Set(['Instant']))
-  const [maxCMC, setMaxCMC] = useState(20)
+  const [maxCMC, setMaxCMC] = useState(4)
   const [colors, setColors] = useState(new Set(['C', 'R', 'G', 'B', 'U', 'W']))
 
+  const getMatches = (types, maxCMC, colors) => {
+    return cards.filter(c => c.cmc <= maxCMC && hasType(c, types) && hasColor(c, colors))
+  }
+
   const renderMatches = (types, maxCMC, colors) => {
+    const matches = getMatches(types, maxCMC, colors)
+    const images = []
+    for (let i = maxCMC; i > 0; i--) {
+      images.push(renderMatchesByCMC(matches, i))
+    }
+    return images
+  }
+
+  const filterByCMC = (elements, CMC) => {
+    return elements.filter(e => e.cmc === CMC)
+  }
+
+  const renderImagesByRarity = (cards) => {
+    const commons = cards.filter(c => c.rarity === 'common')
+    const uncommons = cards.filter(c => c.rarity === 'uncommon')
+    const raresAndMythics = cards.filter(c => ['rare', 'mythic'].includes(c.rarity))
+    return (
+      <div>
+        {
+          commons.length > 0
+            ? (
+              <div>
+                <Typography
+                  variant='h6'
+                  style={{ fontSize: 16 }}
+                  gutterBottom
+                >
+                  {commons.length === 1 ? '1 COMMON' : `${commons.length} COMMONS`}
+                </Typography>
+                {renderImages(commons)}
+              </div>
+              )
+            : ''
+        }
+        {
+          uncommons.length > 0
+            ? (
+              <div>
+                <Typography
+                  variant='h6'
+                  style={{ fontSize: 16 }}
+                  gutterBottom
+                >
+                  {uncommons.length === 1 ? '1 UNCOMMON' : `${uncommons.length} UNCOMMONS`}
+                </Typography>
+                {renderImages(uncommons)}
+              </div>
+              )
+            : ''
+        }
+        {
+          raresAndMythics.length > 0
+            ? (
+              <div>
+                <Typography
+                  variant='h6'
+                  style={{ fontSize: 16 }}
+                  gutterBottom
+                >
+                  {raresAndMythics.length === 1
+                    ? raresAndMythics[0].rarity === 'rare' ? '1 RARE' : '1 MYTHIC'
+                    : `${raresAndMythics.length} RARES AND MYTHICS`}
+                </Typography>
+                {renderImages(raresAndMythics)}
+              </div>
+              )
+            : ''
+        }
+      </div>
+    )
+  }
+
+  const renderImages = (cards) => {
     return cards.map((c, i) => {
-      if (c.cmc <= maxCMC && hasType(c, types) && hasColor(c, colors)) {
-        return (
-          <div key={i}>{c.name}
-            <img src={c.image} height={300} alt={c.name} />
-          </div>
-        )
-      }
+      return (
+        <img
+          key={i}
+          style={{ marginRight: 20, marginBottom: 20 }}
+          src={c.image}
+          alt={c.name}
+        />
+      )
     })
+  }
+
+  const renderMatchesByCMC = (elements, CMC) => {
+    const filteredCards = filterByCMC(elements, CMC).sort((a, b) => a.name.localeCompare(b.name))
+    return (
+      <div style={{ marginBottom: 30 }}>
+        <Typography variant='h5' style={{ fontWeight: 600 }} gutterBottom>CMC {CMC}</Typography>
+        {filteredCards.length > 0 ? renderImagesByRarity(filteredCards) : 'No cards'}
+      </div>
+    )
   }
 
   const toggleColor = (color) => {
@@ -107,96 +232,118 @@ function App () {
     setColors(newColors)
   }
 
+  console.log(getMatches(types, maxCMC, colors))
   return (
-    <div>
-      <b>Instants</b>
-      <div>
-        {colors.has('W')
-          ? (
-            <StyledWhiteIconButton onClick={() => toggleColor('W')} color='primary' aria-label='White Mana' component='span'>
-              <White width={50} />
-            </StyledWhiteIconButton>
-            )
-          : (
-            <IconButton onClick={() => toggleColor('W')} color='primary' aria-label='White Mana' component='span'>
-              <White width={50} />
-            </IconButton>
-            )}
-        {colors.has('U')
-          ? (
-            <StyledBlueIconButton onClick={() => toggleColor('U')} color='primary' aria-label='Blue Mana' component='span'>
-              <Blue width={50} />
-            </StyledBlueIconButton>
-            )
-          : (
-            <IconButton onClick={() => toggleColor('U')} color='primary' aria-label='Blue Mana' component='span'>
-              <Blue width={50} />
-            </IconButton>
-            )}
-        {colors.has('B')
-          ? (
-            <StyledBlackIconButton onClick={() => toggleColor('B')} color='primary' aria-label='Black Mana' component='span'>
-              <Black width={50} />
-            </StyledBlackIconButton>
-            )
-          : (
-            <IconButton onClick={() => toggleColor('B')} color='primary' aria-label='Black Mana' component='span'>
-              <Black width={50} />
-            </IconButton>
-            )}
-        {colors.has('R')
-          ? (
-            <StyledRedIconButton onClick={() => toggleColor('R')} color='primary' aria-label='Red Mana' component='span'>
-              <Red width={50} />
-            </StyledRedIconButton>
-            )
-          : (
-            <IconButton onClick={() => toggleColor('R')} color='primary' aria-label='Red Mana' component='span'>
-              <Red width={50} />
-            </IconButton>
-            )}
-        {colors.has('G')
-          ? (
-            <StyledGreenIconButton onClick={() => toggleColor('G')} color='primary' aria-label='Green Mana' component='span'>
-              <Green width={50} />
-            </StyledGreenIconButton>
-            )
-          : (
-            <IconButton onClick={() => toggleColor('G')} color='primary' aria-label='Green Mana' component='span'>
-              <Green width={50} />
-            </IconButton>
-            )}
-        {colors.has('C')
-          ? (
-            <StyledColorlessIconButton onClick={() => toggleColor('C')} color='primary' aria-label='Colorless Mana' component='span'>
-              <Colorless width={50} />
-            </StyledColorlessIconButton>
-            )
-          : (
-            <IconButton onClick={() => toggleColor('C')} color='primary' aria-label='Colorless Mana' component='span'>
-              <Colorless width={50} />
-            </IconButton>
-            )}
-      </div>
-
-      <Button variant='contained' color='primary' onClick={() => setTypes(new Set(['Instant']))}>Instant</Button>
-      <Button variant='contained' color='primary' onClick={() => setTypes(new Set(['Sorcery']))}>Sorcery</Button>
-      <Slider
-        defaultValue={8}
-        aria-labelledby='discrete-slider-always'
-        step={1}
-        marks
-        min={0}
-        max={8}
-        valueLabelDisplay='on'
-        onChange={(_, v) => {
-          if (v !== maxCMC) {
-            setMaxCMC(v)
-          }
-        }}
-      />
+    <Container>
+      <Typography variant='h3' gutterBottom>What instants could my opponent have?</Typography>
+      <OptionsContainer>
+        <ButtonContainer>
+          <ColorsHeader>
+            <Tooltip title='All colors are selected by default' placement='top-start'>
+              <Typography variant='h6' gutterBottom>COLORS</Typography>
+            </Tooltip>
+            <Button
+              size='small'
+              variant='outlined'
+              color='default'
+              style={{ marginLeft: 20 }}
+              onClick={() => setColors(new Set())}
+            >Clear
+            </Button>
+          </ColorsHeader>
+          {colors.has('W')
+            ? (
+              <StyledWhiteIconButton onClick={() => toggleColor('W')} color='primary' aria-label='White Mana' component='span'>
+                <White width={50} />
+              </StyledWhiteIconButton>
+              )
+            : (
+              <IconButton onClick={() => toggleColor('W')} color='primary' aria-label='White Mana' component='span'>
+                <White width={50} />
+              </IconButton>
+              )}
+          {colors.has('U')
+            ? (
+              <StyledBlueIconButton onClick={() => toggleColor('U')} color='primary' aria-label='Blue Mana' component='span'>
+                <Blue width={50} />
+              </StyledBlueIconButton>
+              )
+            : (
+              <IconButton onClick={() => toggleColor('U')} color='primary' aria-label='Blue Mana' component='span'>
+                <Blue width={50} />
+              </IconButton>
+              )}
+          {colors.has('B')
+            ? (
+              <StyledBlackIconButton onClick={() => toggleColor('B')} color='primary' aria-label='Black Mana' component='span'>
+                <Black width={50} />
+              </StyledBlackIconButton>
+              )
+            : (
+              <IconButton onClick={() => toggleColor('B')} color='primary' aria-label='Black Mana' component='span'>
+                <Black width={50} />
+              </IconButton>
+              )}
+          {colors.has('R')
+            ? (
+              <StyledRedIconButton onClick={() => toggleColor('R')} color='primary' aria-label='Red Mana' component='span'>
+                <Red width={50} />
+              </StyledRedIconButton>
+              )
+            : (
+              <IconButton onClick={() => toggleColor('R')} color='primary' aria-label='Red Mana' component='span'>
+                <Red width={50} />
+              </IconButton>
+              )}
+          {colors.has('G')
+            ? (
+              <StyledGreenIconButton onClick={() => toggleColor('G')} color='primary' aria-label='Green Mana' component='span'>
+                <Green width={50} />
+              </StyledGreenIconButton>
+              )
+            : (
+              <IconButton onClick={() => toggleColor('G')} color='primary' aria-label='Green Mana' component='span'>
+                <Green width={50} />
+              </IconButton>
+              )}
+          {colors.has('C')
+            ? (
+              <StyledColorlessIconButton onClick={() => toggleColor('C')} color='primary' aria-label='Colorless Mana' component='span'>
+                <Colorless width={50} />
+              </StyledColorlessIconButton>
+              )
+            : (
+              <IconButton onClick={() => toggleColor('C')} color='primary' aria-label='Colorless Mana' component='span'>
+                <Colorless width={50} />
+              </IconButton>
+              )}
+        </ButtonContainer>
+        {/* <Button variant='contained' color='primary' onClick={() => setTypes(new Set(['Instant']))}>Instant</Button>
+      <Button variant='contained' color='primary' onClick={() => setTypes(new Set(['Sorcery']))}>Sorcery</Button> */}
+        <ManaCostContainer>
+          <Tooltip title='Generally, how much mana your opponent has up' placement='top-start'>
+            <Typography style={{ marginBottom: 32 }} variant='h6' gutterBottom>MAX CMC</Typography>
+          </Tooltip>
+          <SliderContainer>
+            <Slider
+              defaultValue={4}
+              aria-labelledby='discrete-slider-always'
+              step={1}
+              marks
+              min={0}
+              max={8}
+              valueLabelDisplay='on'
+              onChange={(_, v) => {
+                if (v !== maxCMC) {
+                  setMaxCMC(v)
+                }
+              }}
+            />
+          </SliderContainer>
+        </ManaCostContainer>
+      </OptionsContainer>
       {renderMatches(types, maxCMC, colors)}
-    </div>
+    </Container>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles'
+import CssBaseline from '@material-ui/core/CssBaseline'
 import './index.css'
 import App from './App'
 
+const theme = createMuiTheme({
+  palette: {
+    type: 'dark'
+  }
+})
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ThemeProvider>,
   document.getElementById('root')
 )
